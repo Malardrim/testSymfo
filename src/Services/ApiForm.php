19f3843c610ce79a377ab97297a6f10c0153f604
Marc PAZAT <marc.pazat.ozon@gmail.com>
2019-11-12 17:59:56 +0100

    Documentation on deserializer

--- src/Services/ApiForm.php
+++ src/Services/ApiForm.php
@@ -10,20 +10,40 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiForm
 {
+    /**
+     * @var ValidatorInterface $validator Auto wired service by symfony
+     */
     private $validator;
+
+    /**
+     * @var SerializerInterface $serializer Auto wired service by symfony
+     */
     private $serializer;
 
+    /**
+     * ApiForm constructor.
+     * @param ValidatorInterface $validator
+     * @param SerializerInterface $serializer
+     */
     public function __construct(ValidatorInterface $validator, SerializerInterface $serializer)
     {
         $this->serializer = $serializer;
         $this->validator = $validator;
     }
 
-    public function validateAndCreate($data, $entityClassName)
+    /**
+     * Deserializes then validates à entity
+     *
+     * @param $data string Data to deserialize
+     * @param $entityClassName string Name of the entity to process
+     * @param string $format Format of the data (json recommended)
+     * @return array|object Result of the process if the Entity is valid
+     * @throws CustomApiException
+     */
+    public function validateAndCreate($data, $entityClassName, $format = 'json')
     {
-        $result = $this->serializer->deserialize($data, $entityClassName, 'json');
+        $result = $this->serializer->deserialize($data, $entityClassName, $format);
         $errors = $this->validator->validate($result);
-        dump($errors);
         if (count($errors) > 0) {
             throw new CustomApiException(Response::HTTP_BAD_REQUEST, (string)$errors);
         }
